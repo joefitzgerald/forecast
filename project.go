f4b4b6c57e2469d2db96c7cdea6c7a879058e65d
Daniel Senff <daniel.senff@whataventure.com>
2025-08-14 13:44:47 +0200

    add /projects/:id endpoint
    Adds the /projects/:id endpoint with tests. Also adds missing fields in the projects JSON.

--- project.go
+++ project.go
@@ -2,6 +2,8 @@ package forecast
 
 import (
 	"encoding/csv"
+	"errors"
+	"fmt"
 	"io"
 	"strconv"
 	"strings"
@@ -12,24 +14,30 @@ type projectsContainer struct {
 	Projects Projects `json:"projects"`
 }
 
+type projectContainer struct {
+	Project Project `json:"project"`
+}
+
 // Projects is a list of projects
 type Projects []Project
 
 // Project is a Forecast project
 type Project struct {
-	ID          int       `json:"id"`
-	Name        string    `json:"name"`
-	Color       string    `json:"color"`
-	Code        string    `json:"code"`
-	Notes       string    `json:"notes"`
-	StartDate   string    `json:"start_date"`
-	EndDate     string    `json:"end_date"`
-	HarvestID   int       `json:"harvest_id"`
-	Archived    bool      `json:"archived"`
-	UpdatedAt   time.Time `json:"updated_at"`
-	UpdatedByID int       `json:"updated_by_id"`
-	ClientID    int       `json:"client_id"`
-	Tags        []string  `json:"tags"`
+	ID              int       `json:"id"`
+	Name            string    `json:"name"`
+	Color           string    `json:"color"`
+	Code            string    `json:"code"`
+	Notes           string    `json:"notes"`
+	StartDate       string    `json:"start_date"`
+	EndDate         string    `json:"end_date"`
+	HarvestID       int       `json:"harvest_id"`
+	Archived        bool      `json:"archived"`
+	BudgetBy        string    `json:"budget_by"`
+	BudgetIsMonthly bool      `json:"budget_is_monthly"`
+	UpdatedAt       time.Time `json:"updated_at"`
+	UpdatedByID     int       `json:"updated_by_id"`
+	ClientID        int       `json:"client_id"`
+	Tags            []string  `json:"tags"`
 }
 
 // Projects returns the list of projects in the Forecast Account
@@ -38,7 +46,20 @@ func (api *API) Projects() (Projects, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	return container.Projects, nil
+}
+
+// Project returns the project with the requested ID
+func (api *API) Project(id int) (*Project, error) {
+	if id == 0 {
+		return nil, errors.New("cannot retrieve a project with an id of 0")
+	}
+	container, err := get[projectContainer](api, fmt.Sprintf("projects/%v", id))
+	if err != nil {
+		return nil, err
+	}
+	return &container.Project, nil
 }
 
 // ToCSV writes the projects to the supplied writer in CSV
@@ -55,6 +76,8 @@ func (projects Projects) ToCSV(w io.Writer) error {
 		"end_date",
 		"harvest_id",
 		"archived",
+		"budget_by",
+		"budget_is_monthly",
 		"updated_at",
 		"updated_by_id",
 		"client_id",
@@ -76,6 +99,8 @@ func (projects Projects) ToCSV(w io.Writer) error {
 		record = append(record, project.EndDate)
 		record = append(record, strconv.Itoa(project.HarvestID))
 		record = append(record, strconv.FormatBool(project.Archived))
+		record = append(record, project.BudgetBy)
+		record = append(record, strconv.FormatBool(project.BudgetIsMonthly))
 		record = append(record, project.UpdatedAt.UTC().Format(time.RFC822Z))
 		record = append(record, strconv.Itoa(project.UpdatedByID))
 		record = append(record, strconv.Itoa(project.ClientID))
---
+++ project_test.go
@@ -0,0 +1,119 @@
+package forecast_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/joefitzgerald/forecast"
+	. "github.com/onsi/gomega"
+	"github.com/sclevine/spec"
+	"github.com/sclevine/spec/report"
+)
+
+func TestProject(t *testing.T) {
+	spec.Run(t, "Project", testProject, spec.Report(report.Terminal{}))
+}
+
+func testProject(t *testing.T, when spec.G, it spec.S) {
+	var (
+		server  *httptest.Server
+		handler http.Handler
+		api     *forecast.API
+	)
+
+	it.Before(func() {
+		RegisterTestingT(t)
+		server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if handler != nil {
+				handler.ServeHTTP(w, r)
+			}
+		}))
+
+		api = forecast.New(server.URL, "test-token", "987654")
+	})
+
+	it.After(func() {
+		api = nil
+		if server == nil {
+			return
+		}
+		server.Close()
+		server = nil
+	})
+
+	when("Projects()", func() {
+
+		when("when a response is returned from the server", func() {
+			it.Before(func() {
+				handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					response := ReadFile("projects.json")
+					w.Header().Set("Content-Type", "application/json; charset=utf-8")
+					w.WriteHeader(http.StatusOK)
+					fmt.Fprintf(w, "%s", response)
+				})
+			})
+
+			it("should return projects and a nil error", func() {
+				projects, err := api.Projects()
+				Expect(projects).ShouldNot(BeNil())
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+		})
+
+		when("when an error is returned from the server", func() {
+			it.Before(func() {
+				handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					response := "error"
+					w.Header().Set("Content-Type", "application/json; charset=utf-8")
+					w.WriteHeader(http.StatusBadRequest)
+					fmt.Fprintf(w, "%s", response)
+				})
+			})
+
+			it("should return an error", func() {
+				projects, err := api.Projects()
+				Expect(projects).Should(BeNil())
+				Expect(err).Should(HaveOccurred())
+			})
+		})
+	})
+
+	when("Project()", func() {
+
+		when("when a response is returned from the server", func() {
+			it.Before(func() {
+				handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					response := ReadFile("project.json")
+					w.Header().Set("Content-Type", "application/json; charset=utf-8")
+					w.WriteHeader(http.StatusOK)
+					fmt.Fprintf(w, "%s", response)
+				})
+			})
+
+			it("should return projects and a nil error", func() {
+				projects, err := api.Project(111111)
+				Expect(projects).ShouldNot(BeNil())
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+		})
+
+		when("when an error is returned from the server", func() {
+			it.Before(func() {
+				handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					response := "error"
+					w.Header().Set("Content-Type", "application/json; charset=utf-8")
+					w.WriteHeader(http.StatusBadRequest)
+					fmt.Fprintf(w, "%s", response)
+				})
+			})
+
+			it("should return an error", func() {
+				projects, err := api.Project(111111)
+				Expect(projects).Should(BeNil())
+				Expect(err).Should(HaveOccurred())
+			})
+		})
+	})
+}
